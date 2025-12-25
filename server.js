ba90b9dc3e4786f50b3993406b919241994ffc69
HumeraAshraf110 <humeraashraf203@gmail.com>
2025-12-25 22:07:06 +0500

    initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import {initDB} from "./config/db.js";
+import rateLimiter from "./middleware/rateLimiter.js";
+
+import transactionsRoute from "./routes/transactionsRoute.js";
+
+dotenv.config();
+
+const app = express();
+//middleware
+app.use(rateLimiter);
+app.use(express.json());
+//our custom simple middleware
+// app.use((req,res,next)=>{
+//     console.log("Hey we hit a req,the method is ",req.method);
+//     next();
+// });
+const PORT = process.env.PORT || 5001;
+
+ 
+
+app.get("/",(req, res)=>{
+    res.send("it's working");
+});
+app.use("/api/transactions",transactionsRoute);
+ 
+
+initDB().then(() =>{ 
+app.listen(5001,()=>{
+    console.log("Server is up and running on PORT:",PORT)
+});
+});
