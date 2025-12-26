8c63e9ca6b484c36b19af1fb9f08c92e384725f6
HumeraAshraf110 <humeraashraf203@gmail.com>
2025-12-26 17:54:37 +0500

    Push src folder and package files only

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
-import {initDB} from "./config/db.js";
-import rateLimiter from "./middleware/rateLimiter.js";
+import {initDB} from "../config/db.js";
+import rateLimiter from "../middleware/rateLimiter.js";
 
-import transactionsRoute from "./routes/transactionsRoute.js";
+import transactionsRoute from "../routes/transactionsRoute.js";
 
 dotenv.config();
 
